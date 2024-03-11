24330c99ee828a880260a0fd511c8b66b81805fd
Jasmin Causevic <jcausevi@gmail.com>
2024-03-11 19:35:36 +0100

    initial react webshop commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Header } from './components/Header/Header.jsx'
+import { MainComponent } from './components/MainComponent/MainComponent.jsx'
+import { Footer } from './components/Footer/Footer.jsx'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainComponent />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MainComponent/MainComponent.jsx
@@ -0,0 +1,11 @@
+
+function MainComponent(){
+  return (
+    <div>
+      <h2>Main component part</h2>
+      <p>Here will be the list of products or single product page</p>
+    </div>
+  )
+}
+
+export { MainComponent }
